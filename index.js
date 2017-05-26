4a490edd34adb9f25ec7d1bde43ca5185adc57a6
Allen Kleiner <allenk@pinterest.com>
2017-05-25 17:45:44 -0700

    Add green/red coloring for errors

--- index.js
+++ index.js
@@ -4,19 +4,40 @@ const chalk = require('chalk');
 const pass = `✅`;
 const error = `❌`;
 
+const Base = mocha.reporters.Base;
+
 function EmojiReporter(runner) {
-  mocha.reporters.Base.call(this, runner);
+  Base.call(this, runner);
   let passes = 0;
   let failures = 0;
+  let currentIndentation = 0;
+
+  const indent = () => {
+    return new Array(currentIndentation).join('    ');
+  };
+
+  runner.on('suite', (suite) => {
+    currentIndentation++;
+    console.log(`%s ${chalk.bold.inverse(suite.title)}`, indent());
+  });
+
+  runner.on('suite end', (suite) => {
+    console.log();
+    currentIndentation--;
+  })
 
   runner.on('pass', function(test){
     passes++;
-    console.log(`Test passed ${pass}: %s`, test.fullTitle());
+    currentIndentation++;
+    console.log(`%s ${chalk.green.bold(`Test passed ${pass}: `)} %s`, indent(), test.fullTitle());
+    currentIndentation--;
   });
 
   runner.on('fail', function(test, err){
     failures++;
-    console.log(`Test failed ${error}: %s -- error: %s`, test.fullTitle(), err.message);
+    currentIndentation++;
+    console.log(`%s ${chalk.red.bold(`Test failed ${error}: `)} %s -- error: %s`, indent(), test.fullTitle(), err.message);
+    currentIndentation--;
   });
 
   runner.on('end', function(){
